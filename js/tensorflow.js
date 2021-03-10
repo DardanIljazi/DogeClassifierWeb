97f5ac16728b7b1908c776415e4abedbcbcedc48
Dardan Iljazi <dardan.iljazi@gmail.com>
2021-03-10 15:50:51 +0100

    Prepare for release

--- js/tensorflow.js
+++ js/tensorflow.js
@@ -11,7 +11,7 @@ let classes = ["NOT doge", "doge"]
 window.onload = async function () {
 
     let alreadyPreheated = false
-    const model = await tfconv.loadGraphModel('http://localhost:63342/web/tjfs2/model.json', {
+    const model = await tfconv.loadGraphModel('https://dogeclassifier.com/tjfs2/model.json', {
         
         onProgress: (fraction) => {
             let event = new CustomEvent("loadTfModelProgress", {"detail": fraction})
